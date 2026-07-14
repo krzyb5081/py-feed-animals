21607769b4cbf6b1533ed7ac3fa55d9302b3c339
Krzysztof Sobczak <krzysztof.sobczak5081@gmail.com>
2026-07-14 12:34:54 +0200

    Implement Dog class

--- app/main.py
+++ app/main.py
@@ -21,4 +21,15 @@ class Cat(Animal):
 
     def catch_mouse() -> None;
          print("The hunt began!")
-    
+
+
+class Dog(Animal):
+    def __init__(self, name: str, is_hungry: bool = True):
+        super().__init__(name, 7, is_hungry)
+
+    def bring_slippers() -> None:
+        print("The slippers delivered!")
+
+
+def feed_animals(animals: list) -> int:
+    return sum(animal.feed() for animal in animals)
