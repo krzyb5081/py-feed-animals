236690e913362c201a801402aa6b0a9a865b8720
Krzysztof Sobczak <krzysztof.sobczak5081@gmail.com>
2026-07-14 12:08:57 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
 #  write your code here
+class Animal:
+    def __init__(self, name: str, apetite: int, is_hungry: bool) -> None:
+        self.name = name
+        self.apetite = apetite
+        self.is_hungry = is_hungry
+      
