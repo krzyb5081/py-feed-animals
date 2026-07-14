ccc9a2ebdb9ee00768bc2e775bb39e52feb0823e
Krzysztof Sobczak <krzysztof.sobczak5081@gmail.com>
2026-07-14 12:20:34 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
-#  write your code here
 class Animal:
-    def __init__(self, name: str, apetite: int, is_hungry: bool) -> None:
+    def __init__(self, name: str, appetite: int, is_hungry: bool) -> None:
         self.name = name
-        self.apetite = apetite
+        self.appetite = appetite
         self.is_hungry = is_hungry
-      
+
+    def print_name(self) -> None:
+        print(f"Hello, I'm {self.name}")
+
+    def feed(self) -> int:
+        if self.is_hungry:
+            print(f"Eating {self.appetite} food points...")
+            self.is_hungry = False
+            return self.appetite
+        return 0
+        
